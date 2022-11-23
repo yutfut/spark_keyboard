fa20f30c4859810ec5092011314e3cf039fbf820
yut_fut <seregey.golubev@mail.ru>
2022-11-23 18:06:34 +0300

    edite cursor

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const Scene = require('Scene');
 const TouchGestures = require('TouchGestures');
+const Diagnostics = require('Diagnostics');
+const TimeModule = require('Time');
+
+function sleep(ms) {
+    return new Promise(resolve => TimeModule.setTimeout(resolve, ms));
+}
+
+// function sleep (time) {
+//     return new Promise((resolve) => setTimeout(resolve, time));
+// }
 
 (async function () {
+    const time = 150
     const a = await Scene.root.findFirst('letter_a')
     const b = await Scene.root.findFirst('letter_b')
     const c = await Scene.root.findFirst('letter_c')
@@ -35,126 +46,270 @@ const TouchGestures = require('TouchGestures');
     const textObject = await Scene.root.findFirst('3dText0')
 
     let string1;
-    string1 = "";
+    string1 = "|";
 
     await TouchGestures.onTap(a).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "A"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(b).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "B"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(c).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "C"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(d).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "D"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(e).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "E"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(f).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "F"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(g).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "G"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(h).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "H"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(i).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "I"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(j).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "J"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(k).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "K"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(l).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "L"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(m).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "M"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(n).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "N"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(o).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "O"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(p).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "P"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(r).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "R"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(s).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "S"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(t).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "T"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(u).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "U"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(w).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "W"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(x).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "X"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(y).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "Y"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(z).subscribe(() => {
+        string1 = string1.substring(0, string1.length - 1)
+        textObject.text = string1
         string1 = string1 + "Z"
         textObject.text = string1
+        string1 = string1 + "|"
+        sleep(time).then(() => {
+            textObject.text = string1
+        });
     });
 
     await TouchGestures.onTap(space).subscribe(() => {
